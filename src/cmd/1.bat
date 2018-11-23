f5c4cb01397eb05e1995b70466ba0363a9e9d682
liuyzhm <liuyzhm@yonyou.com>
2018-11-23 20:23:35 +0800

    bat脚本实现js文件压缩

--- src/cmd/1.bat
+++ src/cmd/1.bat
@@ -3,14 +3,15 @@
 
 :: 千万注意， %cd% 指的是，当前dos窗口中执行命令时所在的路径， 并不是该bat文件所在的路径！！！！！！
 :: 设置压缩JS文件的根目录，脚本会自动按树层次查找和压缩所有的JS
-SET JSFOLDER= %cd%\js
+SET JSFOLDER= %cd%\src\js
 echo 正在查找JS文件
 :: 改变当前目录到 %JSFOLDER%
 chdir /d %JSFOLDER%
-:: for /r 递归遍历每个目录，执行for语句
+:: for /r 递归遍历每个目录，执行for语句; “.”表示当前目录
 for /r . %%a in (*.js) do (
     @echo 正在压缩 %%~a ...
 	:: 当启用混淆功能时，如果你希望保留一些名字不被混淆，你可以用--reserved (-r) 声明一些名字，用逗号隔开。例如：uglifyjs ... -m -r '$,require,exports', 防止require, exports和 $被混淆改变。
+    :: 调用以下命令： uglifyjs demo.js -m -o demo.js 实现将demo.js 的文件混淆压缩后，输出到demo.js(即用压缩后的代码，覆盖压缩前的代码)
     uglifyjs %%~fa  -m -o %%~fa
 )
 echo 完成!
--- src/cmd/runNode.js
+++ src/cmd/runNode.js
@@ -9,15 +9,15 @@ const compasspath = path.resolve(__dirname,"../js");
  */
 (function uglifyjsFun(compasspath){
     files = fs.readdirSync(compasspath);
-    console.log("files",files);
+    // console.log("files",files);
     files.forEach(function(item){
         var newPath =  path.resolve(compasspath, item);
-        console.log("newPath",newPath);
+        // console.log("newPath",newPath);
         if(fs.statSync(newPath).isDirectory()){
-            console.log("isDirectory")
+            // console.log("isDirectory")
             uglifyjsFun(newPath); // 递归
         }else if(path.extname(newPath) == ".js"){ 
-            console.log("newPath.extname",newPath);
+            // console.log("newPath.extname",newPath);
             var newName = newPath.replace(".js", ""); 
             // 执行 “ uglifyjs demo.js -m -o demo.min.js” 实现将demo.js 压缩混淆 并将结果输出到demo.min.js
             cp.exec(`uglifyjs ${newPath} -m -o ${newName}.min.js`, function(error, stdout, stderr) {
